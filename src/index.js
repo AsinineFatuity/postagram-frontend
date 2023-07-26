1bafb1287204b646e63b0b29cdf7c09a6626b4bb
AsinineFatuity <asininefatuity@gmail.com>
2023-07-26 09:15:21 +0300

    set up bootstrap, strict mode for index.js for warnings

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Creating the root application component
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
