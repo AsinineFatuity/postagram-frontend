713568e22d6ab4a77dea533d044f9b6b6f49641f
AsinineFatuity <asininefatuity@gmail.com>
2023-07-31 09:18:04 +0300

    made home page to be a protected route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import {Route,Routes} from "react-router-dom";
-import './App.css';
-import ProtectedRoute from './routes/ProtectedRoute';
-import Home from './pages/Home';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import Home from "./pages/Home";
+import Counter from "./pages/Calculator";
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={
-      <ProtectedRoute>
-         <Home/>
-      </ProtectedRoute>   
-      }/>
-      <Route path="/login/" element={<div>Login</div>}/>
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="/login/" element={<div>Login</div>} />
+      <Route path="/counter" element={<Counter />}></Route>
     </Routes>
   );
 }
