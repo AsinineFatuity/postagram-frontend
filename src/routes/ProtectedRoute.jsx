00145fe2beaafe297c22b35f0ae67f8573e9791f
AsinineFatuity <asininefatuity@gmail.com>
2023-08-08 09:51:53 +0300

    fixed redirect issues for authenticated users

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -3,6 +3,6 @@ import { Navigate } from "react-router-dom";
 
 function ProtectedRoute({ children }) {
   const { user } = JSON.parse(localStorage.getItem("auth"));
-  return user.account ? <>{children}</> : <Navigate to="/login/" />;
+  return user.id ? <>{children}</> : <Navigate to="/login/" />;
 }
 export default ProtectedRoute;
