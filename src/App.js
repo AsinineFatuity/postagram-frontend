aaafdc1a319c54a7c9a94ac8648d3d60471e6a04
AsinineFatuity <asininefatuity@gmail.com>
2023-09-15 10:10:44 +0300

    added create comment functionality

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ProtectedRoute from "./routes/ProtectedRoute";
 import Home from "./pages/Home";
 import Registration from "./pages/Registration";
 import Login from "./pages/Login";
+import SinglePost from "./pages/SinglePost";
 
 function App() {
   return (
@@ -16,6 +17,13 @@ function App() {
             <Home />
           </ProtectedRoute>
         }
+      />
+      <Route path="/post/:postId/"
+      element={
+        <ProtectedRoute>
+          <SinglePost/>
+        </ProtectedRoute>
+      }
       />
       <Route path="/register/" element={<Registration/>}></Route>
       <Route path="/login/" element={<Login/>}></Route>
